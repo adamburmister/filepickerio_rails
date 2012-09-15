db16220e434639b907bfa0b059bb3373f6f06d62
Adam Burmister <adam.burmister@gmail.com>
2012-09-15 13:56:21 +0100

    Trying to get the form builder to work

--- lib/filepickerio_rails/action_view/form_builder.rb
+++ lib/filepickerio_rails/action_view/form_builder.rb
@@ -2,8 +2,9 @@ module FilepickerioRails
   class ::ActionView::Helpers::FormBuilder
     
     # Returns a input tag tailored for a Filepicker.io upload widget to be attached for the form object
-    def filepickerio_upload(method, text=nil, options={})
-      # @template.filepickerio_upload_tag(object_name, text, value, objectify_options(options))
+    def fp_file(object_name, method, text, options = {})
+      # The tag helper interface is fp_file_tag(object_name, text=nil, value=nil, options={})
+      # @template.fp_file_tag ...
     end
 
   end
